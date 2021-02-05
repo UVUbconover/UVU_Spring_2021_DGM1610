a57f6d4b9ca0647ea312a6f0d2ee5b0b5763c602
Breana <10657880@my.uvu.edu>
2021-02-04 20:41:30 -0700

    Edit
    Created Top and Lower bound destroy to code.

--- Prototype_2/Assets/Scripts/Destroy_OutofBound.cs
+++ Prototype_2/Assets/Scripts/Destroy_OutofBound.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Destroy_OutofBound : MonoBehaviour
 {
+    public GameObject projectilePrefab;
+    public float topBound;
+    public float lowerBound;
+
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 1);
+        //Destroy(gameObject, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject); }
     }
+    
+    
 }
