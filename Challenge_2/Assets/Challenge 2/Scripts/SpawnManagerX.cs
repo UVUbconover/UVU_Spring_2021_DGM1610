81f36f561f0716a3e823f4706d284ab18ec84324
Breana <10657880@my.uvu.edu>
2021-02-13 06:40:19 -0700

    Bonus of Challenge 2 Completed

--- Challenge_2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Challenge_2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,14 +11,15 @@ public class SpawnManagerX : MonoBehaviour
     private float SpawnPosY = 20;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnInterval = 5.0f;
     
     
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval = Random.Range(3, 5));
     }
 
     // Spawn random ball at random x position at top of play area
