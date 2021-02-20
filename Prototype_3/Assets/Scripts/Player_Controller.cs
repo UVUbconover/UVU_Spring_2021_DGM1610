feb4fe70ed456812366d20e07e098ad48d736854
Breana <10657880@my.uvu.edu>
2021-02-20 07:33:07 -0700

    P3 Edits
    Wrote jumping script.

---
+++ Prototype_3/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityMod;
+    public bool isOnGround = true;
+
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter( Collision other)
+    {
+        isOnGround = true;
+    }
+}
