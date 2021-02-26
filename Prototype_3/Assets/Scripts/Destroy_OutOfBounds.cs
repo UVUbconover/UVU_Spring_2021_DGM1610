34c3420143e4b0b38c2ab067bc415568231780a3
Breana <10657880@my.uvu.edu>
2021-02-25 18:32:20 -0700

    3.1 completed

---
+++ Prototype_3/Assets/Scripts/Destroy_OutOfBounds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_OutOfBounds : MonoBehaviour
+{
+    public Vector3 outOfBounds = new Vector3(-10, 0, 0);
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype_3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(31,0,0);
+    public float startDelay = 1.0f;
+    public float repeatRate = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
