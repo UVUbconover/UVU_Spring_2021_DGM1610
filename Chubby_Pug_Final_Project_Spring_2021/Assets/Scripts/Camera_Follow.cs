bd3ee0c1a1d9d9047ade561d6cead48a5e81fb70
Breana <10657880@my.uvu.edu>
2021-03-08 14:50:47 -0700

    Chubby Pug
    Deleted old code. Rewrote player controls, Wrote a camera follow script. Redid the background so that I can add a repeat/parallax effect code later.

---
+++ Chubby_Pug_Final_Project_Spring_2021/Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public float speed = 10f;
+    public GameObject followPlayer;
+    public Vector2 followPlayerOffset;
+    private Vector2 threshhold;
+    private Rigidbody2D rB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        threshhold = calculateThreshold();
+        rB = followPlayer.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 follow = followPlayer.transform.position;
+        float xDifference = Vector2.Distance(Vector2.right * transform.position.x, Vector2.right * follow.x);
+        float yDifference = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y);
+
+        Vector3 newPosition = transform.position;
+
+        if(Mathf.Abs(xDifference) >= threshhold.x)
+        {
+            newPosition.x = follow.x;
+        }
+
+        if(Mathf.Abs(yDifference) >= threshhold.y)
+        {
+            newPosition.y = follow.y;
+        }
+
+        float moveSpeed = rB.velocity.magnitude > speed ? rB.velocity.magnitude : speed;
+
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, moveSpeed * Time.deltaTime);
+
+    }
+
+    private Vector3 calculateThreshold()
+    {
+        Rect aspect = Camera.main.pixelRect;
+        Vector2 t = new Vector2(Camera.main.orthographicSize * aspect.width / aspect.height, Camera.main.orthographicSize);
+        t.x -= followPlayerOffset.x;
+        t.x -= followPlayerOffset.y;
+        return t;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Vector2 border = calculateThreshold();
+        Gizmos.DrawWireCube(transform.position, new Vector3(border.x * 2, border.y * 2, 1));
+    }
+}
---
+++ Chubby_Pug_Final_Project_Spring_2021/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    public float playerSpeed = 5f;
+    public float hInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        // Controls player movement foward and backward. Meaning left and right across the screen.
+        transform.Translate(Vector3.right * Time.deltaTime * playerSpeed * hInput);
+
+        //Jump command
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(transform.position.y <= -0.25f)
+            {
+                GetComponent<Rigidbody2D>().AddForce(Vector2.up * 325);
+            }   
+        }
+
+    }
+
+}
