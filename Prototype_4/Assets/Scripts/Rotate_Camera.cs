fdc98893f6f3866652716d80be1cf559a57d7bed
Breana <10657880@my.uvu.edu>
2021-03-25 18:47:00 -0600

    Prototype_4_4.2
    Wrote Enemy Script, spawn manager script, and the camera rotation script.

---
+++ Prototype_4/Assets/Scripts/Rotate_Camera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Camera : MonoBehaviour
+{
+    public float rotateSpeed;
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotateSpeed * Time.deltaTime);
+    }
+}
